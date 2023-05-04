d74ab969af6f3535465628605112aafba20411e0
dibyendubiswas1998 <dibyendubiswas1998@gmail.com>
2023-05-04 10:51:25 +0530

    load and save file

---
+++ src/load_and_save.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from src.utils.common_utils import log, save_raw_local_df
+
+def load_data(raw_data_path, log_file):
+    """
+        get the data from Raw Data folder & store to artifacts directory:\n
+        :param raw_data_path: raw_data_path
+        :param directory_path: folder_name
+        :param new_data_path: new_data_path
+        :return: data
+    """
+    try:
+        data = pd.read_csv(raw_data_path, sep=',') # read the data
+        file = log_file
+        log(file_object=file, log_message=f"read the data, shape: {data.shape}")  # logs the details
+        return data
+
+
+    except Exception as e:
+        print(e)
+        file = log_file
+        log(file_object=file, log_message=f"Error will be: {e}")  # logs the error if occurs
+        raise e
+
+def save_data(data, new_data_path, log_file):
+    """
+        It helps to save the data.
+        :param data: data
+        :param new_data_path: new_data_path
+        :return: save data
+    """
+    try:
+        file = log_file
+        data = data
+        save_raw_local_df(data=data, data_path=new_data_path)
+        log(file_object=file, log_message=f"store data in : {new_data_path}")  # logs the details
+        return data  # return data
+
+    except Exception as e:
+        print(e)
+        file = log_file
+        log(file_object=file, log_message=f"Error will be: {e}")  # logs the error if occurs
+        raise e
+
+if __name__ == "__main__":
+    pass
